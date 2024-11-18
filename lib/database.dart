b66d9fd4ae4f026a496d087fa7b49983f1515826
git-edifice <git_edifice@yahoo.co.jp>
2024-11-18 11:42:01 +0900

    Drift - CRUD用のメソッド

--- lib/database.dart
+++ lib/database.dart
@@ -20,6 +20,44 @@ class Database extends _$Database {
 
   @override
   int get schemaVersion => 1;
+   Future insertTodo(String content,DateTime createDatetime) { // 1
+    return into(todos).insert(TodosCompanion.insert(index: 0, content: content, createDatetime: createDatetime));
+  }
+
+  Future deleteTodo(int id) { // 2
+    return (delete(todos)..where((todo) => todo.id.equals(id))).go();
+  }
+
+  Future updateTodo(int id, String content) { // 3
+    return (update(todos)..where((todo) => todo.id.equals(id)))
+        .write(TodosCompanion(content: Value(content)));
+  }  
+
+  Stream<List<Todo>> watchEntries() { // 4
+    // return (select(todos)).watch();
+    return (select(todos)..orderBy([
+     (todos) => OrderingTerm(expression: todos.index, mode: OrderingMode.desc)
+    ])).watch();
+  }
+
+  Future<Todo> getTodo(int id) async { // 5
+    final todo = await (select(todos)..where((todo) => todo.id.equals(id))).getSingle();
+    return todo;
+  }
+
+  //【index(並び順)関連】 --------------------------------------------
+  // 新規レコード作成時にindex をid -1 とする用。
+  Future updateIndex_id1(int id) { // 6
+    return (update(todos)..where((todo) => todo.id.equals(id)))
+        .write(TodosCompanion(index: Value(id -1)));
+  }
+  // indexの交換。
+  Future updateChangeIndex(int id_1,int index_1, int id_2,int index_2) { // 7
+    (update(todos)..where((todo) => todo.id.equals(id_1)))
+        .write(TodosCompanion(index: Value(index_2)));
+    return (update(todos)..where((todo) => todo.id.equals(id_2)))
+        .write(TodosCompanion(index: Value(index_1)));
+  }
 }
 LazyDatabase _openConnection() { // 1
   return LazyDatabase(() async {
