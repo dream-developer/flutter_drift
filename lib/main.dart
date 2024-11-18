ff60087a764d3ec14744c43249d8a64cb937402e
git-edifice <git_edifice@yahoo.co.jp>
2024-11-18 12:32:50 +0900

    Drift - 画面側の作成

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,75 @@
 import 'package:flutter/material.dart';
+import './database.dart'; // 1
+import 'package:intl/intl.dart'; // ロケール。日時表示で使う。
+
+final db = Database();
 
 void main() {
-  runApp(const MyApp());
-}
+  final list = Expanded(
+    child: StreamBuilder( // 2
+      stream: db.watchEntries(), // 3
+      builder: // 4
+          (BuildContext context, AsyncSnapshot<List<Todo>> snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) { // 5
+          return const Center(child: CircularProgressIndicator());
+        }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+        // snapshot.data![i].カラム名 では長いので
+        List<Todo>  tl = snapshot.data ?? []; // 6
+        
+        return ListView.builder( // 7       
+          itemCount: snapshot.data!.length,
+          itemBuilder: (context, i) => ListTile(
+            leading: const Icon(Icons.person),
+            title: Text(tl[i].content), // 8
+            subtitle: Text(  // 9
+              "[ID:${tl[i].id}] ${DateFormat('yyyy-MM-dd HH:mm:ss').format(tl[i].createDatetime)}" 
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            trailing:  Wrap(
+              spacing: 5, // アイコンの間の幅を調整
+              children: [
+                IconButton( // 10
+                  icon: const Icon(Icons.edit),
+                  onPressed: () async {
+                    await db.updateTodo(tl[i].id, '更新');
+                  },
+                ), 
+                IconButton( // 11
+                  icon: const Icon(Icons.delete),
+                  onPressed: () async { 
+                    await db.deleteTodo(tl[i].id);
+                  },
+                ),
+              ],
             ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
+            tileColor: Colors.purple[50], // アイテムの背景色
+          ),
+        );
+      },
+    ),
+  );
+
+  final addButton = ElevatedButton( // 12
+    child: const Text('追加'),
+    onPressed: () async {
+      final insId = await db.insertTodo("追加", DateTime.now()); // 13
+      await db.updateIndex_id1(insId); // 14
+    },
+  );
+
+  final body = SafeArea( // ボディー
+    child: Column(
+      children: [
+        list,
+        addButton,
+      ],
+    )
+  );
+
+  final sc = Scaffold(
+    body: body, // ボディー
+  );
+
+  final app = MaterialApp(home: sc);
+  runApp(app);
 }
